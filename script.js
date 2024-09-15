30f9539fb16d6488329347c72c667102f419c4fe
Ryan <silvallucena02@gmail.com>
2024-09-15 02:33:16 -0300

    corrigindo carrosel E add Btn Menu lateral

---
+++ script.js
@@ -0,0 +1,10 @@
+// Este script e para a barra de navegação lateral abrir e fechar
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
