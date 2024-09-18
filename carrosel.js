2a5b7fbdf5fcf66d2fa1a206d5b0188b7c2c294a
Ryan Lucena Silva <134936752+Ryanlssv@users.noreply.github.com>
2024-09-18 12:14:44 +0000

    ajustes

--- carrosel.js
+++ carrosel.js
@@ -3,32 +3,29 @@ showSlides(slideIndex);
 
 // Criando função para trocar as imgs no carrosel
 
-function plusSlides(n){
-  showSlides(slideIndex += n);
+function plusSlides(n) {
+  showSlides((slideIndex += n));
 }
-
 
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
-  
-  if(n > slides.length){
-    slideIndex  = 1;
+
+  if (n > slides.length) {
+    slideIndex = 1;
   }
 
-  if ( n < 1){
+  if (n < 1) {
     slideIndex = slides.length;
   }
 
-  
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
 
-  slides[slideIndex-1].style.display = "block";
-
+  slides[slideIndex - 1].style.display = "block";
 }
 
-setInterval(function(){
+setInterval(function () {
   plusSlides(1);
 }, 100000);
