b013670cb7f5cdb9f2a6b741303bde50e025cf79
Ryan <silvallucena02@gmail.com>
2024-10-16 11:22:44 -0300

    criando filtro card

--- IndexCardPorduto.js
+++ IndexCardPorduto.js
@@ -1,6 +1,6 @@
 const loja = {
     produtos: [
-        { name:'Capacitador Eltronico', preço:'R$0,86',parcela:'<br> até 10x de R$ 0,085 sem juros <br>sem juros  ou R$ 0,86 via Pix ' ,image:'./imagem/img-Card-maisVendido/img1.png',descricao:'capacitador de 1.000uF/25v <br> Eletrolítico 470uF/50V'},
+        { name:'Capacitador Eltronico', preço:'R$0,86',parcela:'<br> até 10x de R$ 0,085 sem juros <br>sem juros  ou R$ 0,86 via Pix ' ,image:'./imagem/img-Card-maisVendido/img1.png',id:'Componente',descricao:'capacitador de 1.000uF/25v <br> Eletrolítico 470uF/50V'},
         { name:'Módulo Fonte 5V 3W <br> - HLK-PM01', preço:'R$19,95',parcela:'<br> até 10x de R$ 1,99 sem juros <br>sem juros  ou R$ 19,95 via Pix ',image:'./imagem/img-Card-maisVendido/img2.png',descricao:'com entrada bivolt AC de 100 <br>~240VAC, e com saída 5VDC.'},
         { name: 'Arduino Uno R2', preço: 'R$131,50',parcela:'<br> até 10x de R$ 13,50, sem juros <br>sem juros  ou R$ 131,50 via Pix ' , image: './imagem/img-Card-maisVendido/img3.png', descricao:'Microcontrolador: ATmega328 <br> DIPTensão de Entrada: 7-12V Portas  Digitais: 14 Portas Analógicas: 6' },
         { name: 'Ferro de Solda Hikari', preço: 'R$115,71',parcela:'<br> até 10x de R$ 11,571 sem juros <br> 3 sem juros R$ 115,71 via Pix '  ,image: './imagem/img-Card-maisVendido/img4.png', descricao: 'O Ferro de Solda para Estação <br> HK-936A o princípio de funcionamento <br> a transformação de corrente elétrica <br> em calor.' },
