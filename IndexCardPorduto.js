54919d5a013c23ed55ab49b1e76d7707cb4d7b01
Ryan <silvallucena02@gmail.com>
2024-10-21 15:06:59 -0300

    add mais categorias

--- IndexCardPorduto.js
+++ IndexCardPorduto.js
@@ -4,7 +4,7 @@ const loja = {
       { name: 'Módulo Fonte 5V 3W - HLK-PM01', categoria: 'Módulos', preco: 19.95, image: './imagem/img-Card-maisVendido/img2.png',parcela:'<br> até 10x de R$ 1,99 sem juros <br>sem juros  ou R$ 19,95 via Pix ',descricao:'com entrada bivolt AC de 100 <br>~240VAC, e com saída 5VDC.' },
       { name: 'Arduino Uno R2', categoria: 'Arduinos', preco:  131.50,parcela:'<br> até 10x de R$ 13,50, sem juros <br>sem juros  ou R$ 131,50 via Pix ' , image: './imagem/img-Card-maisVendido/img3.png', descricao: 'Microcontrolador: ATmega328 <br> DIPTensão de Entrada: 7-12V Portas  Digitais: 14 Portas Analógicas: 6'},
       { name: 'Motor De Passo NEMA <br> - 57 Kgf.cm / 3,92A', categoria: 'Motores', preco: 436.50,parcela:'<br> até 10x de R$ 43,65, sem juros <br>sem juros  ou R$ 436,65 via Pix ' ,image: './imagem/img-Card-maisVendido/img8.png', descricao: '- Action, Quando pulsos elétricos são aplicados em uma determinada sequência nos terminais deste.' },
-      { name: 'Motor Servo SG90', categoria: 'Motores', preco: 15.00, image: './imagem/img-Card-maisVendido/img4.png', descricao: 'Servo Motor SG90' }
+      { name: 'Ferro de Solda Hikari', categoria: 'Motores', preco: 15.00, image: './imagem/img-Card-maisVendido/img4.png', descricao: 'Servo Motor SG90' }
     ]
   };
   
