37180a4f1c198726786bec905d49484ae862a961
Ryan <silvallucena02@gmail.com>
2024-11-13 00:32:26 -0300

    Criando MODAL produto

--- IndexCardPorduto.js
+++ IndexCardPorduto.js
@@ -1,6 +1,6 @@
 const loja = {
     produtos: [
-      { name: 'Capacitador Elétronico', categoria: 'Componentes',parcela:'até 10x de R$ 0,085 sem juros <br>sem juros  ou R$ 0,86 via Pix ', preco: 0.86, image: './imagem/img-Card-maisVendido/img1.png', descricao: 'capacitador de 1.000uF/25v Eletrolítico 470uF/50V' },
+      { name: 'Capacitador Elétronico', categoria: 'Componentes',parcela:'até 10x de R$ 0,085 sem juros <br>sem juros  ou R$ 0,86 via Pix ', preco: 0.86, image: './imagem/img-Card-maisVendido/img1.png', descricao: 'capacitador de 1.000uF/25v Eletrolítico 470uF/50V',descricaoProdutc: 'O capacitador de 1.000uF/25v tem a função de armazenar energia elétrica para fornecer uma tensão estável com menos variação, usados diversamente no ramo da eletrônica, seja dentro de fontes, placas PCI, computadores entre diversos outros.'   },
       { name: 'Módulo Fonte 5V 3W - HLK-PM01', categoria: 'Módulos', preco: 19.95, image: './imagem/img-Card-maisVendido/img2.png',parcela:'<br> até 10x de R$ 1,99 sem juros <br>sem juros  ou R$ 19,95 via Pix ',descricao:'com entrada bivolt AC de 100 <br>~240VAC, e com saída 5VDC.' },
       { name: 'Arduino Uno R2', categoria: 'Arduinos', preco:  131.50,parcela:'<br> até 10x de R$ 13,50, sem juros <br>sem juros  ou R$ 131,50 via Pix ' , image: './imagem/img-Card-maisVendido/img3.png', descricao: 'Microcontrolador: ATmega328 <br> DIPTensão de Entrada: 7-12V Portas  Digitais: 14 Portas Analógicas: 6'},
       { name: 'Motor De Passo NEMA  23 com redução - 57 Kgf.cm/3,92A Action', categoria: 'Motores', preco: 436.50,parcela:'<br> até 10x de R$ 43,65, sem juros <br>sem juros  ou R$ 436,65 via Pix ' ,image: './imagem/img-Card-maisVendido/img8.png', descricao: '- Action, e uma boa escolha onde há necessidade de um movimento <br> controlado. Como Angulo de rotação' },
@@ -61,14 +61,15 @@ const modal = document.getElementById("productModal");
 const modalImage = document.getElementById("modal-product-image");
 const modalTitle = document.getElementById("modal-product-title");
 const modalDescription = document.getElementById("modal-product-description");
+const modalDescription2 = document.getElementById("modal-descricao-div");
 const modalParcela = document.getElementById("modal-product-parcela");
 const modalPrice = document.getElementById("modal-product-price");
 const closeBtn = document.getElementsByClassName("close")[0];
 
 // Exemplo de dados dos produtos
 const products = [
-  { name: 'Capacitador Elétronico', categoria: 'Componentes',parcela:' até 10x de R$ 0,085 sem juros sem juros  ou R$ 0,86 via Pix ', preco: 0.86, image: './imagem/img-Card-maisVendido/img1.png', descricao: 'capacitador de 1.000uF/25v  Eletrolítico 470uF/50V' },
-  { name: 'Módulo Fonte 5V 3W - HLK-PM01', categoria: 'Módulos', preco: 19.95, image: './imagem/img-Card-maisVendido/img2.png',parcela:' até 10x de R$ 1,99 sem juros sem juros  ou R$ 19,95 via Pix ',descricao:'com entrada bivolt AC de 100 ~240VAC, e com saída 5VDC.' },
+  { name: 'Capacitador Elétronico', categoria: 'Componentes',parcela:' até 10x de R$ 0,085 sem juros sem juros  ou R$ 0,86 via Pix ', preco: 0.86, image: './imagem/img-Card-maisVendido/img1.png', descricao: 'capacitador de 1.000uF/25v  Eletrolítico 470uF/50V' , descricaoProdutc: 'O capacitador de 1.000uF/25v tem a função de armazenar energia elétrica para fornecer uma tensão estável com menos variação, usados diversamente no ramo da eletrônica, seja dentro de fontes, placas PCI, computadores entre diversos outros.'  },
+  { name: 'Módulo Fonte 5V 3W - HLK-PM01', categoria: 'Módulos', preco: 19.95, image: './imagem/img-Card-maisVendido/img2.png',parcela:' até 10x de R$ 1,99 sem juros sem juros  ou R$ 19,95 via Pix ',descricao:'com entrada bivolt AC de 100 ~240VAC, e com saída 5VDC.', descricaoProdutc: 'Módulo Fonte - HLK-PM01 Fonte encapsulada com entrada bivolt e saída retificada 5 VDC com corrente máxima de saída de 600mA.Perfeita para ser utilizada em projetos de Internet das Coisas ou dispositivos compactos que podem ser energizados diretamente.' },
   { name: 'Arduino Uno R2', categoria: 'Arduinos', preco:  131.50,parcela:' até 10x de R$ 13,50, sem juros sem juros  ou R$ 131,50 via Pix ' , image: './imagem/img-Card-maisVendido/img3.png', descricao: 'Microcontrolador: ATmega328 DIPTensão de Entrada: 7-12V Portas  Digitais: 14 Portas Analógicas: 6'},
   { name: 'Motor De Passo NEMA  23 com redução', categoria: 'Motores', preco: 436.50,parcela:' até 10x de R$ 43,65, sem juros sem juros  ou R$ 436,65 via Pix ' ,image: './imagem/img-Card-maisVendido/img8.png', descricao: '- Action, e uma boa escolha onde há necessidade de um movimento controlado. Como Angulo de rotação' },
   { name: 'Ferro de Solda Hikari', categoria: 'Prototipagem', preco: 115.71 ,parcela:' até 10x de R$ 11,571 sem juros  3 sem juros R$ 115,71 via Pix ' , image: './imagem/img-Card-maisVendido/img4.png',  descricao: 'O Ferro de Solda para Estação HK-936A o princípio de funcionamento a transformação de corrente elétrica em calor.' },
@@ -84,6 +85,7 @@ function openModal(product) {
     modalImage.src = product.image;
     modalTitle.textContent = product.name;
     modalDescription.textContent = product.descricao;
+    modalDescription2.textContent = product.descricaoProdutc;
     modalPrice.textContent = "R$ " +  product.preco ;
     modalParcela.textContent = product.parcela;
     modal.style.display = "block";
